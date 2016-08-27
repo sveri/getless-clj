f631660aa31c0216c28078f5f1bca7bcc7b3547a
sveri <sveri@sveri.de>
2016-08-27 08:55:48 +0200

    need to display found products

--- src/clj/de/sveri/getless/routes/off.clj
+++ src/clj/de/sveri/getless/routes/off.clj
@@ -1,10 +1,13 @@
 (ns de.sveri.getless.routes.off
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [routes GET]]
+            [clojure.string :as s]
             [de.sveri.getless.layout :as layout]
+            [de.sveri.getless.service.off :as off]
             [ring.util.response :refer [response]]))
 
-(defn search-page []
-  (layout/render "off/search.html"))
+(defn search-page [{:keys [off-url off-user off-password]} search]
+  (let [products (if-not (s/blank? search) (off/search-products search off-url off-user off-password) {})]
+    (layout/render "off/search.html" {:products (:products products) :search search})))
 
-(defroutes off-routes
-           (GET "/off/search" [] (search-page)))
+(defn off-routes [config]
+  (routes (GET "/off/search" [search] (search-page config search))))
