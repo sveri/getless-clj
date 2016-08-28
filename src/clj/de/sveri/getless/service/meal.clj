bc0ef7814a8002f6144f152faf6c9d0fa4fc8af0
sveri <sveri@sveri.de>
2016-08-28 18:29:39 +0200

    meal session service

--- src/clj/de/sveri/getless/service/meal.clj
+++ src/clj/de/sveri/getless/service/meal.clj
@@ -1,7 +1,25 @@
-(ns de.sveri.getless.service.meal)
+(ns de.sveri.getless.service.meal
+  (:require [clojure.spec :as s]
+            [clojure.spec.test :as stest]))
 
 (def breakfast "BREAKFAST")
-(def )
+(def launch "LAUNCH")
+(def supper "SUPPER")
+(def snack "SNACK")
 
-(defn save-new-meal [session]
+(s/def ::breakfast #(= % breakfast))
+(s/def ::launch #(= % launch))
+(s/def ::supper #(= % supper))
+(s/def ::snack #(= % snack))
+(s/def ::breakfast-type #{::snack ::supper ::launch ::breakfast})
+
+(s/def ::session-map (s/keys))
+
+
+(s/fdef save-new-meal :args (s/cat :session ::session-map :breakfast-type (s/nilable ::breakfast-type))
+        :ret any?)
+(defn save-new-meal [session & [breakfast-type]]
   (assoc-in session [:meal :type] breakfast))
+
+
+(stest/instrument)
