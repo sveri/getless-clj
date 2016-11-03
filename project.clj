9dd1d7d67fd7545aaa411c808a690bfcf2b98161
sveri <sveri@sveri.de>
2016-11-03 07:31:22 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject getless "0.1.0-SNAPSHOT"
+(defproject getless "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
