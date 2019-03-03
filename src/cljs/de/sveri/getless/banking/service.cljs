04ff41305c3788a3c6d9397d59afa7438b90b073
sveri <sveri80@gmail.com>
2019-03-03 16:53:50 +0100

    showing data from this month

--- src/cljs/de/sveri/getless/banking/service.cljs
+++ src/cljs/de/sveri/getless/banking/service.cljs
@@ -3,6 +3,18 @@
             [cljs-time.coerce :as t-c]))
 
 
+(defn get-transactions-for-this-month [transactions]
+  (let [now (t/now)
+        year (t/year now)
+        month (t/month now)
+        year-after (t/year (t/plus now (t/months 1)))
+        month-after (t/month (t/plus now (t/months 1)))]
+    (filter #(t/within? (t/interval (t/date-time year month)
+                                    (t/date-time year-after month-after))
+                        (t-c/from-date (:booking-date %)))
+            transactions)))
+
+
 (defn get-transactions-for-last-month [transactions]
   (let [now (t/now)
         year (t/year now)
@@ -17,5 +29,6 @@
 
 (defn get-transactions-in-time-range [transactions time-range]
   (cond
-    (= "this_month" time-range) (get-transactions-for-last-month transactions)
+    (= "this_month" time-range) (get-transactions-for-this-month transactions)
+    (= "last_month" time-range) (get-transactions-for-last-month transactions)
     :else transactions))
